8f5e915b8faf1ad737b03a1408ffc8ce16146385
Steven Reiss <spr@cs.brown.edu>
2026-04-03 18:11:24 -0400

    Better fix for timing problem.

--- javasrc/edu/brown/cs/bubbles/bddt/BddtBubbleManager.java
+++ javasrc/edu/brown/cs/bubbles/bddt/BddtBubbleManager.java
@@ -108,10 +108,15 @@ BudaBubble createExecBubble(BumpThread bt)
    if (stk == null) return null;
 
    BumpStackFrame usefrm = stk.getFrame(0);
-   if (usefrm == null) {
+   for (int i = 0; i < 10; ++i) {
+      if (usefrm != null) break;
+      BoardLog.logE("BDDT","No stack frame found " + i + " " + stk.getNumFrames());
+      try {
+         Thread.sleep(10);
+       }
+      catch (InterruptedException e) { }
       stk = bt.getStack();
       usefrm = stk.getFrame(0);
-      BoardLog.logE("BDDT","No stack frame found");
     }
    BudaBubble bb = createSourceBubble(stk,0,BubbleType.EXEC,false,godown);
    if (bb == null && stk != null && bddt_properties.getBoolean("Bddt.show.user.bubble")) {
